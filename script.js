f8cb2a2837b5c784d1a9acf0b59e546aac6754a8
Eti <etibhawsar29@gmail.com>
2022-09-18 02:07:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+// console.log("hjd")
+
+const search =()=>{
+    const searchbox =document.getElementById("search-item").value.toUpperCase();
+    const storeitems = document.getElementById("product-list");
+    const product = document.querySelectorAll(".product");
+    const pname = storeitems.getElementsByTagName("h2");
+    
+    for(let i =0; i<pname.length; i++){
+
+    let match = product[i].getElementsByTagName("h2")[0];//yaha par jitne bhi products hai un sab k tag name mtlb h2 vale text mil jayega==================or==================jine bhi products hai un sabhi k h2 aajayenge inske pass match k pass
+    
+    if(match){
+    let textvalue = match.textContent || match.innerHTML;
+
+    if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+        product[i].style.display ="";
+    }else{
+        product[i].style.display ="none";
+    }
+}
+
+}
+
+
+}
